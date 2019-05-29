d4d9b0fc1be2367e5582a401ea1058f44553ecec
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-05-29 17:26:43 +0200

    removed unused modules

--- src/app/views/base/base-routing.module.ts
+++ src/app/views/base/base-routing.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CardsComponent } from './cards.component';
-import { FormsComponent } from './forms.component';
-import { SwitchesComponent } from './switches.component';
-import { TablesComponent } from './tables.component';
-import { TabsComponent } from './tabs.component';
 import { CarouselsComponent } from './carousels.component';
 import { CollapsesComponent } from './collapses.component';
-import { PaginationsComponent } from './paginations.component';
-import {PopoversComponent} from './popovers.component';
-import {ProgressComponent} from './progress.component';
-import {TooltipsComponent} from './tooltips.component';
+
 
 const routes: Routes = [
   {
@@ -32,34 +25,6 @@ const routes: Routes = [
         }
       },
       {
-        path: 'forms',
-        component: FormsComponent,
-        data: {
-          title: 'Forms'
-        }
-      },
-      {
-        path: 'switches',
-        component: SwitchesComponent,
-        data: {
-          title: 'Switches'
-        }
-      },
-      {
-        path: 'tables',
-        component: TablesComponent,
-        data: {
-          title: 'Tables'
-        }
-      },
-      {
-        path: 'tabs',
-        component: TabsComponent,
-        data: {
-          title: 'Tabs'
-        }
-      },
-      {
         path: 'carousels',
         component: CarouselsComponent,
         data: {
@@ -71,34 +36,6 @@ const routes: Routes = [
         component: CollapsesComponent,
         data: {
           title: 'Przedmiot 3 - widok dla studenta'
-        }
-      },
-      {
-        path: 'paginations',
-        component: PaginationsComponent,
-        data: {
-          title: 'Pagination'
-        }
-      },
-      {
-        path: 'popovers',
-        component: PopoversComponent,
-        data: {
-          title: 'Popover'
-        }
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-        data: {
-          title: 'Progress'
-        }
-      },
-      {
-        path: 'tooltips',
-        component: TooltipsComponent,
-        data: {
-          title: 'Tooltips'
         }
       }
     ]
--- src/app/views/base/base.module.ts
+++ src/app/views/base/base.module.ts
@@ -5,16 +5,6 @@ import { NgModule } from '@angular/core';
 
 import { CardsComponent } from './cards.component';
 
-// Forms Component
-import { FormsComponent } from './forms.component';
-
-import { SwitchesComponent } from './switches.component';
-import { TablesComponent } from './tables.component';
-
-// Tabs Component
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TabsComponent } from './tabs.component';
-
 // Carousel Component
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CarouselsComponent } from './carousels.component';
@@ -26,21 +16,6 @@ import { CollapsesComponent } from './collapses.component';
 // Dropdowns Component
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-// Pagination Component
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { PopoversComponent } from './popovers.component';
-
-// Popover Component
-import { PopoverModule } from 'ngx-bootstrap/popover';
-import { PaginationsComponent } from './paginations.component';
-
-// Progress Component
-import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
-import { ProgressComponent } from './progress.component';
-
-// Tooltip Component
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { TooltipsComponent } from './tooltips.component';
 
 
 // Components Routing
@@ -52,26 +27,13 @@ import { BaseRoutingModule } from './base-routing.module';
     FormsModule,
     BaseRoutingModule,
     BsDropdownModule.forRoot(),
-    TabsModule,
     CarouselModule.forRoot(),
     CollapseModule.forRoot(),
-    PaginationModule.forRoot(),
-    PopoverModule.forRoot(),
-    ProgressbarModule.forRoot(),
-    TooltipModule.forRoot()
   ],
   declarations: [
     CardsComponent,
-    FormsComponent,
-    SwitchesComponent,
-    TablesComponent,
-    TabsComponent,
     CarouselsComponent,
-    CollapsesComponent,
-    PaginationsComponent,
-    PopoversComponent,
-    ProgressComponent,
-    TooltipsComponent
+    CollapsesComponent
   ]
 })
 export class BaseModule { }
